6bcd468b530c1621b64d5a548d6dfc468d5adb89
ramosjuliete <ramos.juliete@gmail.com>
2021-12-07 09:43:07 -0300

    Projeto Criado

---
+++ src/main/java/com/zup/juliete_user_comics/JulieteUserComicsApplication.java
@@ -0,0 +1,13 @@
+package com.zup.juliete_user_comics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JulieteUserComicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JulieteUserComicsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zup/juliete_user_comics/JulieteUserComicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zup.juliete_user_comics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JulieteUserComicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
